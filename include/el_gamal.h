627a010df5aea50027e58a64cbf93e4a0047ea48
eric <eric.durr.20@ull.edu.es>
2021-04-27 00:45:32 +0100

    working el gamal

--- include/el_gamal.h
+++ include/el_gamal.h
@@ -57,21 +57,6 @@ unsigned f_exp(unsigned x, unsigned y, unsigned mod)
   return res;
 }
 
-int gcdExtended(int a, int b, int &x, int &y)
-{
-  if (a == 0)
-  {
-    x = 0;
-    y = 1;
-    return b;
-  }
-  int x1, y1;
-  int gcd = gcdExtended(b % a, a, x1, y1);
-  x = y1 - (b / a) * x1;
-  y = x1;
-  return gcd;
-}
-
 unsigned mod_mult_inv(unsigned num, unsigned mod)
 {
   for (unsigned x = 1; x < mod; x++)
